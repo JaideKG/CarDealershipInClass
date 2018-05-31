f7fb73c19fa41c8ef8b66d1691a94b40e3a24c26
Peter F. Guenther <peter.guenther@gmail.com>
2018-05-31 14:22:57 -0400

    first commit

---
+++ CarDealershipInClass/Models/CarDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarDealershipInClass.Models
+{
+    public class CarDAO : IDAO
+    {
+        private CarEntities db;
+
+        public CarDAO ()
+        {
+            db = new CarEntities();
+        }
+
+        public Car GetCar (int id)
+        {
+            return db.Cars.Find(id);
+        }
+
+        public List<Car> GetCarList ()
+        {
+            return db.Cars.ToList();
+        }
+
+        public void AddCar(Car car)
+        {
+            db.Cars.Add(car);
+            db.SaveChanges();
+        }
+
+        public void DeleteCar(int id)
+        {
+            Car car = db.Cars.Find(id);
+            db.Cars.Remove(car);
+            db.SaveChanges();
+        }
+
+        public void EditCar(Car car)
+        {
+            db.Entry(car).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ CarDealershipInClass/Models/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealershipInClass.Models
+{
+    interface IDAO
+    {
+        Car GetCar(int id);
+        
+        List<Car> GetCarList();
+
+        void AddCar(Car car);
+
+        void DeleteCar(int id);
+        
+        void EditCar(Car car);
+
+        void Dispose();
+    }
+}
